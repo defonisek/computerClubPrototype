962c3d59d559f092d0d1ba42bb2c0a121ad4a508
defonisek <defonisek@mail.ru>
2025-05-06 11:48:42 +0300

    feat: basic echo clubDriver w some err checks

--- src/clubDriver.cpp
+++ src/clubDriver.cpp
@@ -18,7 +18,38 @@ ClubDriver::~ClubDriver(){
 }
 
 void ClubDriver::process(std::string &filename){
-    
+    std::ifstream file(filename);
+    std::string line;
+    try{
+        if(!std::getline(file, line))
+            throw std::invalid_argument("");
+        club.tableAmount = parser->numLine(line);
+        if(!std::getline(file, line))
+            throw std::invalid_argument("");
+        std::pair<std::string, std::string> openCloseT = parser->openCloseTime(line);
+        club.openingTime = openCloseT.first;
+        club.closingTime = openCloseT.second;
+        if(!std::getline(file, line))
+            throw std::invalid_argument("");
+        club.hourlyRate = parser->numLine(line);
+    }
+    catch(...){
+        std::cerr << "ERR: Not enough data in the provided file.";
+    }
+    std::cout << club.openingTime;
+    club.tables.reserve(club.tableAmount);
+    for(int i=0;i<club.tableAmount;++i){
+        club.tables.emplace_back(Table{0, std::nullopt, "", i});
+    }
+    club.freeTables = club.tableAmount;
+    clientHandler->setClub(club);
+    std::vector<std::string> event;
+    while(std::getline(file, line)){
+        std::cout << line << "\n";
+        event = parser->parseEvent(line);
+        clientHandler->handle(event);
+    }
+    std::cout << club.closingTime;
 }
 
 
