8f6b9273d200071ab3f95c6872e822a720dcc8fa
defonisek <defonisek@mail.ru>
2025-05-06 20:31:49 +0300

    feat: struct client redundant, map instead

--- src/common.h
+++ src/common.h
@@ -10,12 +10,6 @@
 namespace computerClub{
     using Time = std::string;
 
-	struct Client{
-        std::string name;
-        std::optional<int> currTable;
-        int currID;
-    };
-
 	struct Table{
 		int revenue;
         std::optional<std::string> currClient;
@@ -29,6 +23,7 @@ namespace computerClub{
         std::vector<Table> tables;
         std::queue<std::string> clientLine;
         std::unordered_map<std::string, int> clientTableMap;
+        std::unordered_map<std::string, int> clientIDMap;
 		Time openingTime;
 		Time closingTime;
 		int hourlyRate;	
