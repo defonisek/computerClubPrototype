42f3d73634b22baab512994bc57914adecd71c31
defonisek <defonisek@mail.ru>
2025-05-06 17:14:47 +0300

    feat: struct to manage event gen

--- src/processing/clientHandler.h
+++ src/processing/clientHandler.h
@@ -4,10 +4,17 @@
 #include "../common.h"
 
 namespace computerClub{
+    struct GeneratedEvent{
+        Time time;
+        std::string clientOrMessage;
+        int eventId;
+        std::string additionalParam;
+    };
+
     class ClientHandler{
         public:
-            ClientHandler(){};
-            void handle(std::vector<std::string> &event){};
+            ClientHandler();
+            void handle(std::vector<std::string> &event);
             void setClub(Club club){club_ = club;};
         private:
             Club club_;
